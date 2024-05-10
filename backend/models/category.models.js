d04008d87ce33b0aa8bb171407231ef67cfaae5d
Rahul07-dev <rahuldev072002@gmail.com>
2024-05-11 00:59:59 +0530

    Resolved Schema issues

--- backend/models/category.models.js
+++ backend/models/category.models.js
@@ -1,4 +1,4 @@
-import mongoose from "mongoose";
+import mongoose, {Schema} from "mongoose";
 import { commonStrings } from "../constants";
 
 const categorySchema= new mongoose.Schema({
--- backend/models/seller.models.js
+++ backend/models/seller.models.js
@@ -1,4 +1,4 @@
-import mongoose from "mongoose";
+import mongoose,{Schema}from "mongoose";
 import { userSchema } from "./user.models";
 import { commonStrings } from "../constants";
 
